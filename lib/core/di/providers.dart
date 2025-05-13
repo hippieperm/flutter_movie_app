43c28bd530d2c478f8c2957e30895142d34a3819
hippieperm <leess0133@gmail.com>
2025-05-13 15:01:28 +0900

    영화 데이터 소스를 활용하여 MovieRepositoryImpl 클래스를 제공하는 movieRepositoryProvider 추가

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_movie_app/core/network/dio_provider.dart';
 import 'package:flutter_movie_app/data/datasource/movie_data_source.dart';
 import 'package:flutter_movie_app/data/datasource/movie_data_source_impl.dart';
+import 'package:flutter_movie_app/domain/repository/movie_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final movieDataSourceProvider = Provider<MovieDataSource>((ref) {
   final dio = ref.watch(dioProvider);
   return MovieDataSourceImpl(dio);
+});
+
+// Repository
+final movieRepositoryProvider = Provider<MovieRepository>((ref) {
+  final dataSource = ref.watch(movieDataSourceProvider);
+  return MovieRepositoryImpl(dataSource);
 });
