03730a599513cc9f34a33d7bbe13cd21fba149d0
hippieperm <leess0133@gmail.com>
2025-05-14 02:02:27 +0900

    ✨ FeatureMovie 위젯의 CachedNetworkImage에 로딩 중 표시를 추가하고, 이미지 크기를 명시적으로 설정하여 UI 개선

--- lib/presentation/view/widget/home/feature_movie.dart
+++ lib/presentation/view/widget/home/feature_movie.dart
@@ -14,19 +14,23 @@ class FeatureMovie extends StatelessWidget {
   Widget build(BuildContext context) {
     final heroTag = 'main_feature_${movie.id}';
 
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: GestureDetector(
-        onTap: () {},
-        child: Hero(
-          tag: heroTag,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: CachedNetworkImage(
-              imageUrl: movie.fullPosterPath,
+    return GestureDetector(
+      onTap: () {},
+      child: Hero(
+        tag: heroTag,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: CachedNetworkImage(
+            imageUrl: movie.fullPosterPath,
+            height: 450,
+            width: double.infinity,
+            fit: BoxFit.cover,
+            placeholder: (context, url) => Container(
               height: 450,
-              width: double.infinity,
-              fit: BoxFit.cover,
+              color: Colors.grey.shade800,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
             ),
           ),
         ),
