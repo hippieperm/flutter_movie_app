9556c9cb091463c43dcb17db16cc888307ca0b40
hippieperm <leess0133@gmail.com>
2025-05-14 01:07:41 +0900

    상세 페이지를 위한 새로운 위젯 ClassName 추가: 기본 Scaffold 구조 구현

---
+++ lib/presentation/view/detail_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ClassName extends ConsumerWidget {
+  const ClassName({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
