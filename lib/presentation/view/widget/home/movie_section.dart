f7c26f7fcde1e2e8806f99cd7e518dc72cba538d
hippieperm <leess0133@gmail.com>
2025-05-14 01:37:05 +0900

    MovieSection 위젯에 제목과 영화 목록을 위한 필드 추가: 생성자에서 필수 매개변수로 설정

--- lib/presentation/view/widget/home/movie_section.dart
+++ lib/presentation/view/widget/home/movie_section.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app/domain/entity/movie.dart';
 
 class MovieSection extends StatelessWidget {
-  const MovieSection({super.key});
+  final String title;
+  final List<Movie> movies;
+
+  const MovieSection({
+    super.key,
+    required this.title,
+    required this.movies,
+  });
 
   @override
   Widget build(BuildContext context) {
