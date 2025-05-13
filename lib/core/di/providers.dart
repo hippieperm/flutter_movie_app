74e9ecbe7090977f0632f7d13a015523dc9f8b3b
hippieperm <leess0133@gmail.com>
2025-05-13 14:57:23 +0900

    영화 데이터 소스를 위한 의존성 주입을 위해 필요한 패키지 추가

--- lib/core/di/providers.dart
+++ lib/core/di/providers.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_movie_app/core/network/dio_provider.dart';
+import 'package:flutter_movie_app/data/datasource/movie_data_source.dart';
+import 'package:flutter_movie_app/data/datasource/movie_data_source_impl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final movieDataSourceProvider = Provider<MovieDataSource>((ref) {
