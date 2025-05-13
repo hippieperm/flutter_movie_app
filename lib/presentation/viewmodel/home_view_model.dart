e32415e7ecf44a7dd28aed05fcdc205d616a9b2f
hippieperm <leess0133@gmail.com>
2025-05-13 15:21:38 +0900

    홈 화면 상태 관리를 위한 HomeState 클래스 추가

---
+++ lib/presentation/viewmodel/home_view_model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_movie_app/domain/entity/movie.dart';
+
+class HomeState {
+  final List<Movie>? nowPlayingMovies;
+  final List<Movie>? popularMovies;
+  final List<Movie>? topRatedMovies;
+  final List<Movie>? upcomingMovies;
+  final bool isLoading;
+  final String? error;
+
+  HomeState({
+    required this.nowPlayingMovies,
+    required this.popularMovies,
+    required this.topRatedMovies,
+    required this.upcomingMovies,
+    required this.isLoading,
+    required this.error,
+  });
+}
