5d254f361e5476cfb866821457ec24db42b18331
hippieperm <leess0133@gmail.com>
2025-05-14 01:42:18 +0900

    MovieSection 위젯에 수평 스크롤 가능한 영화 목록 추가: CachedNetworkImage를 사용하여 영화 포스터를 표시하고, 로딩 및 오류 처리를 구현

--- lib/presentation/view/widget/home/movie_section.dart
+++ lib/presentation/view/widget/home/movie_section.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_app/domain/entity/movie.dart';
 
@@ -30,6 +31,50 @@ class MovieSection extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 10),
+        SizedBox(
+          height: 180,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length > 20 ? 20 : movies.length,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            itemBuilder: (context, index) {
+              final movie = movies[index];
+              final heroTag = '${sectionId}_${movie.id}';
+
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Hero(
+                    tag: heroTag,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: CachedNetworkImage(
+                        imageUrl: movie.fullPosterPath,
+                        width: 120,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) => Container(
+                          width: 120,
+                          color: Colors.grey.shade800,
+                          child: const Center(
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2.0,
+                            ),
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => Container(
+                          width: 120,
+                          color: Colors.grey.shade800,
+                          child: const Center(child: Icon(Icons.error)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ],
     );
   }
