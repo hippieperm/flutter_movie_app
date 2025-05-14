c94b4231d10ec0e1546f555d70881d0af7af7472
hippieperm <leess0133@gmail.com>
2025-05-14 10:18:51 +0900

    영화 세부 정보 표시를 위한 StatItem 위젯 추가: 레이블과 값을 시각적으로 표현하는 컨테이너 구현

---
+++ lib/presentation/view/widget/detail/stat_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class StatItem extends StatelessWidget {
+  final String label;
+  final String value;
+
+  const StatItem({
+    super.key,
+    required this.label,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 110,
+      margin: const EdgeInsets.only(right: 12),
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade800,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            value,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(fontSize: 14, color: Colors.grey.shade400),
+          ),
+        ],
+      ),
+    );
+  }
+}
