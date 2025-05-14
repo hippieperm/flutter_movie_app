9628384868a3528d163aa6efb628108fc5eb6865
hippieperm <leess0133@gmail.com>
2025-05-14 10:22:51 +0900

    영화 세부 정보 페이지를 위한 DetailPage 위젯 구현: 영화 ID와 선택적 heroTag를 포함하여 소비자 위젯으로 구성

--- lib/presentation/view/detail_page.dart
+++ lib/presentation/view/detail_page.dart
@@ -1,8 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ClassName extends ConsumerWidget {
-  const ClassName({super.key});
+class DetailPage extends ConsumerWidget {
+  final int movieId;
+  final String? heroTag;
+
+  const DetailPage({
+    super.key,
+    required this.movieId,
+    this.heroTag,
+  });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
