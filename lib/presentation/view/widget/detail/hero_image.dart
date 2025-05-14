bb56bfa448a7a278a45e8535d1d5e9a4905fab6d
hippieperm <leess0133@gmail.com>
2025-05-14 10:19:28 +0900

    영화 세부 정보 표시를 위한 HeroImage 위젯 추가: 캐시된 네트워크 이미지를 사용하여 이미지 로딩 및 오류 처리를 구현

---
+++ lib/presentation/view/widget/detail/hero_image.dart
@@ -0,0 +1,38 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class HeroImage extends StatelessWidget {
+  final int movieId;
+  final String imageUrl;
+  final String? heroTag;
+
+  const HeroImage({
+    super.key,
+    required this.movieId,
+    required this.imageUrl,
+    this.heroTag,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: heroTag ?? 'movie_$movieId',
+      child: _buildImageWidget(imageUrl),
+    );
+  }
+
+  Widget _buildImageWidget(String imageUrl) {
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      fit: BoxFit.cover,
+      placeholder: (context, url) => Container(
+        color: Colors.grey.shade800,
+        child: const Center(child: CircularProgressIndicator()),
+      ),
+      errorWidget: (context, url, error) => Container(
+        color: Colors.grey.shade800,
+        child: const Center(child: Icon(Icons.error)),
+      ),
+    );
+  }
+}
